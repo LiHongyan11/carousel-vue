5a42df825c1e1057240df2e0b85c66c532db8acc
LiHongyan11 <179947177@qq.com>
2018-07-21 15:55:36 +0800

    feature

--- README.md
+++ README.md
@@ -1,21 +1,79 @@
-# vue-test
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Development](#development)
+- [License](#license)
 
-## Project setup
-```
-npm install
-```
+## Installation
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
+``` bash
+npm install carousel-vue
 ```
 
-### Lints and fixes files
+or if you prefer yarn
+
+``` bash
+yarn add carousel-vue
 ```
-npm run lint
+
+## Usage
+
+Include the carousel directly into your component using import:
+
+``` js
+import { Carousel, CarouselItem } from 'carousel-vue';
+
+export default {
+  ...
+  components: {
+    Carousel,
+    CarouselItem
+  }
+  ...
+};
 ```
+
+### Configuration
+| Property                  | Type    | Default | Description                                                                                                                                                                                                                                                                           |
+|:--------------------------|:--------|:--------|:--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| visibleItem                    | Number  | 7    | visible item number                                                                                                                                                                                   |
+| reduce                    | Number  | 10       | adjoin item reduce px  
+
+### HTML Structure
+
+Once the **Carousel** and **Slide** components are installed globally or imported, they can be used in templates in the following manner:
+
+``` html
+  <carousel>
+    <carousel-item :index="0">
+      item 1 Content
+    </carousel-item>
+    <carousel-item :index="1">
+      item 2 Content
+    </carousel-item>
+  </carousel>
+```
+
+## Development
+
+A sandboxed dev environment is provided by [vue-play](https://github.com/vue-play/vue-play). Changes made to the component files will appear in real time in the sandbox. 
+
+To begin development, run:
+
+``` bash
+npm install 
+npm run dev
+```
+
+or, if you prefer yarn
+
+``` bash
+yarn install 
+yarn dev
+```
+
+then navigate to `http://localhost:8080`
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -12,15 +12,11 @@
 export default {
   name: 'Carousel',
   props: {
-    scaling: {
+    reduce: {
       type: Number,
       default: 10
     },
-    borderRadius: {
-      type: String,
-      default: '15px'
-    },
-    showNum: {
+    visibleItem: {
       type: Number,
       default: 7,
     }
@@ -30,22 +26,22 @@ export default {
       itemOptions: {
         maxSize: '100px',
       },
-      activeIndex: Math.floor(this.showNum / 2),
+      activeIndex: Math.floor(this.visibleItem / 2),
       showList: [],
       list: 0
     }
   },
   mounted() {
-    if (this.isLessShowNum) { // 如果实际展示list小于showNum, 将list的长度重置为showNum, 并改变activeIndex的计算基准为实际list的长度
+    if (this.isLessShowNum) { // 如果实际展示list小于visibleItem, 将list的长度重置为visibleItem, 并改变activeIndex的计算基准为实际list的长度
       this.activeIndex = Math.floor(this.getChildrenLength / 2)
-      this.list = this.showNum; 
+      this.list = this.visibleItem; 
     } else this.list = this.getChildrenLength;
     this.getMaxSize();
   },
 
   computed: {
     isLessShowNum() {
-      return  this.getChildrenLength < this.showNum;
+      return  this.getChildrenLength < this.visibleItem;
     },
     getChildrenLength() {
       return this.$children[0].$children.length;
@@ -54,39 +50,39 @@ export default {
 
   methods: {
     getMaxSize() {
-      const {showNum, scaling} = this;
+      const {visibleItem, reduce} = this;
 
       const ulWidth = document.querySelector('.list').clientWidth;
       let total = 0;
-      for(let i = 1; i <= Math.floor(showNum / 2); i++) {
-        total += i * scaling * 2;
+      for(let i = 1; i <= Math.floor(visibleItem / 2); i++) {
+        total += i * reduce * 2;
       }
-      if (showNum % 2 === 0 || (this.isLessShowNum && this.getChildrenLength % 2 === 0)) { // 如果展示个数为偶数，则所有item一样大小
-        this.itemOptions.maxSize = (ulWidth - (showNum - 1) * 5) / showNum;
+      if (visibleItem % 2 === 0 || (this.isLessShowNum && this.getChildrenLength % 2 === 0)) { // 如果展示个数为偶数，则所有item一样大小
+        this.itemOptions.maxSize = (ulWidth - (visibleItem - 1) * 5) / visibleItem;
       } else {
-        this.itemOptions.maxSize = (ulWidth - (showNum - 1) * 5 + total) / showNum; //(showNum - 1) * 5: 中间margin-right： 5px
+        this.itemOptions.maxSize = (ulWidth - (visibleItem - 1) * 5 + total) / visibleItem; //(visibleItem - 1) * 5: 中间margin-right： 5px
       }
 
       this.updateShowList();
     },
     updateShowList() {
-      const { scaling, list, itemOptions, showNum } = this;
+      const { reduce, list, itemOptions, visibleItem } = this;
       for (let i = 0; i < list; i ++) { // 默认填充为0
         this.showList.splice(i, 1, 0);
       }
 
-      if (showNum % 2 === 0 || (this.isLessShowNum && this.getChildrenLength % 2 === 0)) {
+      if (visibleItem % 2 === 0 || (this.isLessShowNum && this.getChildrenLength % 2 === 0)) {
         this.$set(this.showList, this.activeIndex, itemOptions.maxSize);
         this.$set(this.showList, this.activeIndex + 1, itemOptions.maxSize);
-        for (let i = 1; i <= Math.floor(showNum / 2) - 1; i++) { // Math.floor(showNum / 2) - 1: 减去中间已经设置好的两张
+        for (let i = 1; i <= Math.floor(visibleItem / 2) - 1; i++) { // Math.floor(visibleItem / 2) - 1: 减去中间已经设置好的两张
           this.$set(this.showList, this.activeIndex - i, Math.floor(itemOptions.maxSize));
           this.$set(this.showList, this.activeIndex + i + 1, Math.floor(itemOptions.maxSize));
         }
       } else {
         this.$set(this.showList, this.activeIndex, itemOptions.maxSize);
-        for (let i = 1; i <= Math.floor(showNum / 2); i++) {
-          this.$set(this.showList, this.activeIndex - i, Math.floor(itemOptions.maxSize - i * scaling));
-          this.$set(this.showList, this.activeIndex + i, Math.floor(itemOptions.maxSize - i * scaling));
+        for (let i = 1; i <= Math.floor(visibleItem / 2); i++) {
+          this.$set(this.showList, this.activeIndex - i, Math.floor(itemOptions.maxSize - i * reduce));
+          this.$set(this.showList, this.activeIndex + i, Math.floor(itemOptions.maxSize - i * reduce));
         }
       }
       
